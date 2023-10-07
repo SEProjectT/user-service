6e45f3d73bfedfe472029e2d79eab5d433f073c3
Andrey <khalyavinandrey@gmail.com>
2023-10-07 12:53:40 +0500

    обновил user service

--- src/main/kotlin/user_service/demo/service/UserService.kt
+++ src/main/kotlin/user_service/demo/service/UserService.kt
@@ -1,26 +1,12 @@
 package user_service.demo.service
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import user_service.demo.dto.UserDto
-import user_service.demo.repository.UserRepository
 
-@Service
-@Transactional(readOnly = true)
-class UserService(@Autowired val userRepository: UserRepository) {
+interface UserService {
 
-    fun getUser(id: Long): Mono<UserDto> {
-        return userRepository.findById(id)
-            .map { UserDto(it.id, it.username, it.preferredContact, it.email, it.phone) }
-            .log()
-    }
+    fun getUser(id: Long): Mono<UserDto>
 
-    fun getUsers(ids: List<Long>): Flux<UserDto> {
-        return userRepository.findByIds(ids)
-            .map { UserDto(it.id, it.username, it.preferredContact, it.email, it.phone) }
-            .log()
-    }
+    fun getUsers(ids: List<Long>): Flux<UserDto>
 }
