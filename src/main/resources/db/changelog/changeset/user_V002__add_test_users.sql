ae929ff66a349364dd90d52daca616c1c7b2e77d
andrey.khalyavin <andrey.khalyavin@ural.rt.ru>
2023-12-24 14:50:16 +0500

    ready

--- src/main/resources/db/changelog/changeset/user_V002__add_test_users.sql
+++ src/main/resources/db/changelog/changeset/user_V002__add_test_users.sql
@@ -2,5 +2,5 @@ INSERT INTO users (username, preferred_contact, email, phone)
 VALUES
     ('user1', 'SMS', 'test@test.ru', 'phone1'),
     ('user2', 'EMAIL', 'fk-00@list.ru', 'phone2'),
-    ('user3', 'TELEGRAM', 'team@team.ru', 'phone3')
+    ('user3', 'EMAIL', 'team@team.ru', 'phone3')
 ON CONFLICT DO NOTHING;
